1ca593d6600e39ddb021608df6c196f9bedceb12
RohitV11 <69058845+RohitV11@users.noreply.github.com>
2022-04-08 20:29:15 -0700

    Update gameLogic.js

--- my-game/_js/gameLogic.js
+++ my-game/_js/gameLogic.js
@@ -1,32 +1,58 @@
+// global variables
 let canvas = null;
 let ctx = null;
 
-let WIDTH = 1024;
-let HEIGHT = 768;
+let TILESIZE = 64;
+let TILESWIDE = 22;
+let TILESHIGH = 9;
+let WIDTH = TILESWIDE * TILESIZE;
+let HEIGHT = TILESHIGH * TILESIZE;
 let BGCOLOR = "blue";
-let TILESIZE = 32;
 
 let allSprites = [];
 let allWalls = [];
 let allCacti = [];
-let keysDown = {};
-let keysUp = {}; 
 
+let keysDown = {};
+let keysUp = {};
+
+// utility function for displaying text 
+
+function drawText(r, g, b, a, font, align, base, text, x, y) {
+    ctx.fillStyle = "rgba(" + r + "," + g + "," + b + "," + a + ")";
+    ctx.font = font;
+    ctx.textAlign = align;
+    ctx.textBaseline = base;
+    ctx.fillText(text, x, y);
+}
+
+
+/* ###################### Event listeners that wait for keyboard input ############### */
+
+addEventListener("keydown", function (event) {
+    // keysDown = {};
+    keysDown[event.key] = true;
+    // console.log(event);
+}, false);
+
+addEventListener("keyup", function (event) {
+    keysUp[event.key] = true;
+    delete keysDown[event.key];
+    // console.log("the key that was removed " + event);
+}, false);
+
+
+/* ###################### String the we read to build the level ############### */
 let gamePlan = `
-.........................#####..........
-........................................
-......######............................
-......######............................
-......######............................
-........................................
-........................................
-........................................
-.#####################..................
-...#####################................
-.................###############........
-........................................
-..........................##############
-...........................#############`;
+#............|........
+.............|........
+.............|........
+.|...........|........
+.|.|.......#####......
+..|...................
+..#............#......
+####..################
+####..################`;
 
 // this is like a MOLD
 class Sprite {
@@ -39,10 +65,10 @@ class Sprite {
         this.speed = 3;
         allSprites.push(this);
     }
-    create(x,y,w,h) {
+    create(x, y, w, h) {
         return new Sprite(x, y, w, h)
     }
-    collideWith(obj){
+    collideWith(obj) {
         if (this.x + this.w >= obj.x &&
             this.x <= obj.x + obj.w &&
             this.y + this.h >= obj.y &&
@@ -59,8 +85,8 @@ class Sprite {
     }
 }
 
-class Wall extends Sprite {
-    constructor (x, y, w, h, color) {
+class Wall extends Sprite {  //using mold to create wall
+    constructor(x, y, w, h, color) {
         super(x, y, w, h, color);
         this.x = x;
         this.y = y;
@@ -69,13 +95,13 @@ class Wall extends Sprite {
         this.color = 'rgb(255, 0, 0)';
         allWalls.push(this);
     }
-    create(x,y,w,h) {
+    create(x, y, w, h) {
         return new Wall(x, y, w, h)
     }
 }
 
-class Cactus extends Sprite {
-    constructor (x, y, w, h, color) {
+class Cactus extends Sprite { //using mold to create catus
+    constructor(x, y, w, h, color) {
         super(x, y, w, h, color);
         this.x = x;
         this.y = y;
@@ -84,51 +110,85 @@ class Cactus extends Sprite {
         this.color = 'rgb(0, 255, 0)';
         allCacti.push(this);
     }
-    create(x,y,w,h) {
+    create(x, y, w, h) {
         return new Cactus(x, y, w, h)
     }
 }
 
-class Player extends Sprite {
+class Player extends Sprite { //using mold to create player that is modifiable
     constructor(x, y, w, h, color) {
         super(x, y, w, h, color);
         this.x = x;
         this.y = y;
+        this.vx = 0;
+        this.vy = 0;
+        this.speed = 5;
+        this.gravity = 9.8;
+        this.coFriction = 1;
         this.w = w;
         this.h = h;
         this.color = color;
-        this.speed = 10;
+        this.health = 100;
+        this.jumpPower = 50;
+        this.canJump = true;
+        this.invulnerable = false;
         allSprites.push(this);
     }
+    frictionX() { //cutting down velocity because of friction
+        if (this.vx > 0.5) {
+            this.vx -= this.coFriction;
+        } else if (this.vx < -0.5) {
+            this.vx += this.coFriction;
+        } else {
+            this.vx = 0;
+        }
+    }
+    jump() { //changing y velocity 
+        this.vy = -this.jumpPower;
+        console.log(this.timesJumped);
+        console.log(this.vy);
+    }
     input() {
-        if ("w" in keysDown) {
-            this.y-=this.speed;
+        // if ("w" in keysDown) {
+        //     this.y-=this.speed;
+        // }
+        if (" " in keysDown && this.canJump) {
+            // this.vy = -1;
+            this.canJump = false;
+            this.jump();
+            console.log("trying to jump...");
         }
         if ("a" in keysDown) {
-            this.x-=this.speed;
+            this.vx -= this.speed;
         }
-        if ("s" in keysDown) {
-            this.y+=this.speed;
-        }
+        // if ("s" in keysDown) {
+        //     this.y+=this.speed;
+        // }
         if ("d" in keysDown) {
-            this.x+=this.speed;
+            this.vx += this.speed;
         }
     }
     // adding updates....
     update() {
+        console.log(this.x);
+        this.vy += this.gravity;
         this.input();
-        if (this.x > WIDTH-this.w){
-            this.x = WIDTH-this.w;
-         }
-        if (this.x < 0){
+        this.frictionX();
+        this.x += Math.floor(this.vx);
+        this.y += this.vy;
+        if (this.x > WIDTH - this.w) {
+            this.x = WIDTH - this.w;
+        }
+        if (this.x < 0) {
             this.x = 0;
-         }
-        if (this.y > HEIGHT - this.h){
+        }
+        if (this.y > HEIGHT - this.h) {
             this.y = HEIGHT - this.h;
-         }
-        if (this.y < 0){
+            this.canJump = true;
+        }
+        if (this.y < 0) {
             this.y = 0;
-         }
+        }
     }
     draw() {
         ctx.fillStyle = this.color;
@@ -146,11 +206,11 @@ class Circle {
         this.speed = 2;
     }
     update() {
-        this.x += this.speed*Math.random()*5;
-        this.y += this.speed*Math.random()*5;
-        if (this.x > WIDTH || this.x < 0 || this.y < 0 || this.y > HEIGHT){
-               this.speed*=-1; 
-            }
+        this.x += this.speed * Math.random() * 5;
+        this.y += this.speed * Math.random() * 5;
+        if (this.x > WIDTH || this.x < 0 || this.y < 0 || this.y > HEIGHT) {
+            this.speed *= -1;
+        }
     }
     draw() {
         ctx.beginPath();
@@ -163,12 +223,12 @@ class Circle {
 
 
 
-function makeGrid(plan, width) {
+function makeGrid(plan, width) { //creating game board
     let newGrid = [];
     let newRow = [];
 
-    for (i of plan){
-        if (i != "\n"){
+    for (i of plan) {
+        if (i != "\n") {
             newRow.push(i);
         }
         if (newRow.length % width == 0 && newRow.length != 0) {
@@ -181,7 +241,7 @@ function makeGrid(plan, width) {
 }
 
 function readLevel(grid) {
- let startActors = [];
+    let startActors = [];
     for (y in grid) {
         for (x in grid[y]) {
 
@@ -193,7 +253,7 @@ function readLevel(grid) {
                     startActors.push(type);
                 } else {
                     let t = new type;
-                    startActors.push(t.create(x*TILESIZE, y*TILESIZE, TILESIZE, TILESIZE))
+                    startActors.push(t.create(x * TILESIZE, y * TILESIZE, TILESIZE, TILESIZE))
                 }
                 // console.log(startActors);
             }
@@ -218,10 +278,10 @@ console.log(allWalls);
 // creates a div; sets attributes; appends body; creates canvas; puts canvas inside div
 function init() {
     let gameDiv = document.createElement("div");
-    gameDiv.setAttribute("style", "border: 1px solid;"
-    + "width:" + WIDTH + "px; "
-    + "height:" + HEIGHT + "px; "
-    + "background-color: " + BGCOLOR);
+    gameDiv.setAttribute("style", "border: 1px solid;" +
+        "width:" + WIDTH + "px; " +
+        "height:" + HEIGHT + "px; " +
+        "background-color: " + BGCOLOR);
     document.body.appendChild(gameDiv);
 
     canvas = document.createElement("canvas");
@@ -230,11 +290,11 @@ function init() {
 
     ctx = canvas.getContext('2d');
     document.body.appendChild(canvas);
-    
+
     try {
         gameDiv.appendChild(canvas);
         console.log("game initialized");
-    } catch (e){
+    } catch (e) {
         alert(e.message);
     }
     gameLoop();
@@ -242,55 +302,60 @@ function init() {
 
 
 
-let player = new Player(WIDTH/2, HEIGHT/2, 64, 64, 'rgb(255, 255, 0)');
+let player = new Player(WIDTH / 2, HEIGHT / 2, TILESIZE, TILESIZE, 'rgb(255, 255, 0)');
 console.log(allSprites);
+// let spongeBob = new Sprite(10, 10, 30, 30, 'rgb(255, 255, 0)');
+// let patrick = new Sprite(10, 30, 65, 65, 'rgb(255, 150, 150)');
+// let squidward = new Sprite(70, 90, 20, 20, 'rgb(0, 200, 200)');
+// let sandy = new Circle(70, 200, 25, 40, 'rgb(150, 75, 0)');
 
 function update() {
     player.update();
-    for (i of allCacti){
-        if (i.collideWith(player)){
+    for (c of allCacti) {
+        if (c.collideWith(player)) {
             console.log("ouch...");
+            if (!player.invulnerable) {
+                player.health-=10;
+                player.invulnerable = true;
+                setTimeout(() => {
+                    player.invulnerable = false
+                }, 2000);
+            }
+
+        }
+    }
+    for (w of allWalls) {
+        if (w.collideWith(player)) {
+            console.log("ouch...");
+            player.y = w.y - player.h;
+            player.vy = 0;
+            player.canJump = true;
         }
     }
 
 }
 
-function draw() {
+function draw() { //creating an element
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    for (i of allSprites){
+    for (i of allSprites) {
         i.draw();
     }
     drawText(0, 0, 0, 1, "20px Helvetica", "left", "top", "FPS: " + fps, TILESIZE / 2, TILESIZE / 2);
+    drawText(255, 255, 255, 1, "20px Helvetica", "left", "top", player.health, TILESIZE / 2, 64);
 
 }
 
+let fps;
+let then = performance.now();
 
-function gameLoop(){
-    console.log('alive!!!');
+function gameLoop() {
+    // console.log('the game loop is alive!!!');
+    now = performance.now();
+    let delta = now - then;
+    fps = (Math.ceil(1000 / delta));
+    // totaltime = now - then;
+    then = now;
     update();
     draw();
     window.requestAnimationFrame(gameLoop);
-}
-function init() {
-    let gameDiv = document.createElement("div");
-    gameDiv.setAttribute("style", "border: 1px solid;"
-    + "width:" + WIDTH + "px; "
-    + "height:" + HEIGHT + "px; "
-    + "background-color: " + BGCOLOR);
-    document.body.appendChild(gameDiv);
-
-    canvas = document.createElement("canvas");
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
-
-    ctx = canvas.getContext('2d');
-    document.body.appendChild(canvas);
-    
-    try {
-        gameDiv.appendChild(canvas);
-        console.log("game initialized");
-    } catch (e){
-        alert(e.message);
-    }
-    gameLoop();
 }
